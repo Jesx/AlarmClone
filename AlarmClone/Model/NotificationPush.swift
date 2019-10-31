82307b26f0b86248b60849b939d89132fe0626c4
Jes <ilamtibr@gmail.com>
2019-10-31 16:13:43 +0800

    minor code revised

--- AlarmClone/Model/NotificationPush.swift
+++ AlarmClone/Model/NotificationPush.swift
@@ -28,47 +28,39 @@ class NotificationPush {
 //        content.sound = UNNotificationSound.default
         content.sound = UNNotificationSound.init(named:UNNotificationSoundName(rawValue: "\(alarm.ringTone).mp3"))
         content.categoryIdentifier = NotificationCategory.AlarmNotification.rawValue
-
+        
         let hour = alarm.time.hour
         let min = alarm.time.min
         
+        var identifier = ""
+        var dateComponent = DateComponents()
+        
         if alarm.repeatStatus.count == 0 {
-            let identifier = alarm.uuid
-            let dateComponent = DateComponents(calendar: Calendar.current, hour: hour, minute: min, second: 0)
-
-            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
-            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-            
-            UNUserNotificationCenter.current().add(request) { (error) in
-                if let error = error {
-                    print(error.localizedDescription)
-                } else {
-                    print("Notificaion succeed.")
-                }
-            }
+            identifier = alarm.uuid
+            dateComponent = DateComponents(calendar: Calendar.current, hour: hour, minute: min, second: 0)
             
         } else {
             for day in alarm.repeatStatus {
-                let identifier = alarm.uuid + day.rawValue
+                identifier = alarm.uuid + day.rawValue
                 let weekday = day.index + 1
-                let dateComponent = DateComponents(calendar: Calendar.current,
+                dateComponent = DateComponents(calendar: Calendar.current,
                                                    hour: hour,
                                                    minute: min,
                                                    second: 0,
                                                    weekday: weekday)
-            
                 
-                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
-                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request) { (error) in
+            }
+        }
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
 
-                    if let error = error {
-                        print(error.localizedDescription)
-                    } else {
-                        print("Notificaion succeed.")
-                    }
-                }
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Notificaion succeed.")
             }
         }
         
