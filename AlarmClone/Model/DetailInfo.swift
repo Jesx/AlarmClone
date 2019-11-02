b68592587f2427245f2daff9cb3d1365454aacdc
Jes <ilamtibr@gmail.com>
2019-11-02 19:22:16 +0800

    add weekdays to selection and add content to readme

--- AlarmClone/Model/DetailInfo.swift
+++ AlarmClone/Model/DetailInfo.swift
@@ -22,13 +22,14 @@ enum DetailInfo {
     }
     
     enum repeatAdditional {
-        case Never, Everday, Weekend
+        case Never, Everday, Weekend, Weekday
         
         var destription: String {
             switch self {
             case .Never: return "Never"
             case .Everday: return "Every day"
             case .Weekend: return "Weekend"
+            case .Weekday: return "Weekday"
             }
         }
     }
@@ -40,23 +41,27 @@ extension Array where Element == DetailInfo.DaysOfWeek {
         switch self {
         case []:
             return DetailInfo.repeatAdditional.Never.destription
-        case [.Sunday, .Monday, .Tuesday, .Wednesday, .Thusday, .Friday,.Saturday]:
+        case [.Sunday, .Monday, .Tuesday, .Wednesday, .Thusday, .Friday, .Saturday]:
             return DetailInfo.repeatAdditional.Everday.destription
         case [.Sunday, .Saturday]:
-            return DetailInfo.repeatAdditional.Weekend.destription
+            return "\(DetailInfo.repeatAdditional.Weekend.destription)s"
+        case [.Monday, .Tuesday, .Wednesday, .Thusday, .Friday]:
+            return "\(DetailInfo.repeatAdditional.Weekday.destription)s"
         default:
             return map{ $0.rawValue.prefix(3) }.joined(separator: " ")
         }
     }
     
     var uiStringMain: String {
-        switch uiString {
-        case DetailInfo.repeatAdditional.Never.destription:
+        switch self {
+        case []:
             return ""
-        case DetailInfo.repeatAdditional.Everday.destription:
-            return ", \(uiString.lowercased())"
-        case DetailInfo.repeatAdditional.Weekend.destription:
-            return ", every \(uiString.lowercased())"
+        case [.Sunday, .Monday, .Tuesday, .Wednesday, .Thusday, .Friday, .Saturday]:
+            return ", \(DetailInfo.repeatAdditional.Everday.destription.lowercased())"
+        case [.Sunday, .Saturday]:
+            return ", every \(DetailInfo.repeatAdditional.Weekend.destription.lowercased())"
+        case [.Monday, .Tuesday, .Wednesday, .Thusday, .Friday]:
+            return ", every \(DetailInfo.repeatAdditional.Weekday.destription.lowercased())"
         default:
             return ", \(uiString)"
         }
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # AlarmClone
 
+An app that clone original iOS system alarm, which is in the clock app.
+
+## Demo
+
+
+
+
+
 ## TODO
+
 1. Notification
+    - Play the selected music for longer period
     - Snooze
     - Duplicate clock conflict
 
